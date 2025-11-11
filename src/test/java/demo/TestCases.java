5f653751c13645cf4b0cef63e6238cf836eba913
nabarun02 <nabarunpaul02@gmail.com>
2025-11-12 04:47:56 +0530

    Nabarun_QA_XGOOGLE_FORM_MODULE_L1_DONE

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,21 +1,32 @@
 package demo;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Level;
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
 
 public class TestCases {
+
     ChromeDriver driver;
+    WebDriverWait wait;
 
     /*
      * TODO: Write your tests here with testng @Test annotation. 
@@ -56,4 +67,81 @@ public class TestCases {
         driver.quit();
 
     }
+
+    @Test
+    public void testCase01() throws InterruptedException{
+
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        Wrappers wrapper = new Wrappers(driver);
+
+
+        wrapper.navigateToForm();
+
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@jsname = 'YPqjbf']//input[@class = 'whsOnd zHQkBf']")));
+
+        wrapper.enterTextByXpath("//div[@jsname = 'YPqjbf']//input[@class = 'whsOnd zHQkBf']", "Crio Learner");
+
+
+        String epochTime = String.valueOf(System.currentTimeMillis() / 1000);
+        String textToEnter = String.format("I want to be the best QA Engineer! %s", epochTime);
+
+        wrapper.enterTextByXpath("//textarea[@class = 'KHxj8b tL9Q4c']", textToEnter);
+
+
+        List<String> optionsToSelect = Arrays.asList("0 - 2");
+
+        wrapper.selectOptions("//div[@class = 'nWQGrd zwllIb']//div[@class = 'ulDsOb']//span", optionsToSelect);
+
+
+        optionsToSelect = Arrays.asList("Java", "Selenium", "TestNG");
+
+        wrapper.selectOptions("//div[@class = 'eBFwI']//div[@class = 'ulDsOb']//span", optionsToSelect);
+        
+        wrapper.clickElementByXpath("//div[contains(@class, 'DEh1R')]");
+
+        Thread.sleep(500);
+
+        optionsToSelect = Arrays.asList("Mr");
+
+        wrapper.selectOptions("//div[contains(@class, 'QXL7Te')]//div[contains(@class, 'OIC90c ')]//span", optionsToSelect);
+
+        
+        LocalDate sevenDaysBefore = LocalDate.now().minusDays(7);
+
+        String sevenDaysBeforeFormatted = sevenDaysBefore.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+        
+        wrapper.enterTextByXpath("//div[@jsname = 'jPalo']//input[contains(@class, 'whsOnd zHQkBf')]", sevenDaysBeforeFormatted);
+
+
+        LocalTime curTime = LocalTime.now();
+
+        // String hr = curTime.format(DateTimeFormatter.ofPattern("hh"));
+        // String min = String.valueOf(curTime.getMinute());
+        String amPm = curTime.format(DateTimeFormatter.ofPattern("a"));
+
+        wrapper.enterTextByXpath("//div[@jsname = 'MKaSrf']//input[contains(@class, 'whsOnd zHQkBf')]", "07");
+        wrapper.enterTextByXpath("//div[@jsname = 'QbtXXe']//input[contains(@class, 'whsOnd zHQkBf')]", "30");
+
+        wrapper.clickElementByXpath("//div[@jsname = 'CXE57d']//div[@class = 'e2CuFe eU809d']");
+
+        optionsToSelect = Arrays.asList(amPm);
+
+        wrapper.selectOptions("//div[@jsname = 'V68bde']//div[@jsname = 'wQNmvb']//span", optionsToSelect);
+
+        //submitting the form
+        wrapper.clickElementByXpath("//span[normalize-space(text()) = 'Submit']");
+
+        
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class= 'vHW8K']")));
+
+
+        String successMessage = driver.findElement(By.xpath("//div[@class= 'vHW8K']")).getText();
+
+        System.out.println(String.format("Message after form submission: %s", successMessage));
+
+
+    }
+    
 }
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -3,6 +3,7 @@ package demo.wrappers;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
@@ -16,4 +17,79 @@ public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+
+     ChromeDriver driver;
+
+     String formUrl = "https://docs.google.com/forms/d/e/1FAIpQLSep9LTMntH5YqIXa5nkiPKSs283kdwitBBhXWyZdAS-e4CxBQ/viewform?pli=1";
+
+     public Wrappers(ChromeDriver driver){
+        this.driver = driver;
+     }
+
+     public void navigateToForm(){
+
+        if(!driver.getCurrentUrl().equals(formUrl)){
+            driver.get(formUrl);
+        }
+
+     }
+
+     public void enterTextByXpath(String xPath, String text){
+
+         try{
+
+            WebElement textArea = driver.findElement(By.xpath(xPath));
+            
+            textArea.click();
+
+            Thread.sleep(300);
+            
+            textArea.clear();
+            textArea.sendKeys(text);
+      
+         }
+
+         catch(Exception e){
+            System.out.println("Failed to enter text: " + e.getMessage());
+         }
+
+     }
+
+     public void clickElementByXpath(String xPath){
+
+         try{
+            driver.findElement(By.xpath(xPath)).click();
+         }
+
+         catch(Exception e){
+            System.out.println("Failed to click on element: "+ e.getMessage());
+         }
+
+     }
+
+     public void selectOptions(String xPath, List<String> optionsToSelect){
+         
+        List<WebElement> options = driver.findElements(By.xpath(xPath));
+
+        int sz = optionsToSelect.size();
+
+        for(WebElement option : options){
+
+            String optionText = option.getText().trim();
+            
+            if(optionsToSelect.contains(optionText)){
+               
+               option.click();
+               
+               sz--;
+
+               if(sz == 0){
+                  break;
+               }
+
+            }
+
+        }
+     }
+
 }
